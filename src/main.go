246e4a9548b7aa613bec491dd6fb8d6e9841f96b
jettc <jettc@jettc.home>
2023-10-25 13:02:14 -0600

    Working on reading the files

--- src/main.go
+++ src/main.go
@@ -1,21 +1,57 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"log"	
-    "os"
+	"io"
+	"log"
+	"os"
 )
 
+func findKeyWord(word string, doc []string) (wordCound int){
+    for _, v := range doc { 
+        if v == word {
+            fmt.Println(v)
+            wordCound += 1
+        }
+    } 
+    return wordCound
+}
+
+func parsString(str io.Reader) (parsedString []string) {
+    scanner := bufio.NewScanner(str)
+    scanner.Split(bufio.ScanWords)
+
+    for scanner.Scan() {
+        parsedString = append(parsedString, scanner.Text())
+    }
+    
+    return parsedString
+}
+
 func main() {    
-    resume, err := os.ReadFile("../resume/resume")
+    resume, err := os.Open("../resume/resume")
     if err != nil {
         log.Fatal(err)
     }
-    jobDescription, err := os.ReadFile("../jobDescription/jobDescription") 
+    defer resume.Close()
+
+    jobDescription, err := os.Open("../jobDescription/jobDescription ") 
     if err != nil {
         log.Fatal(err)
+    }
+    defer jobDescription.Close()
+
+    parsedResume := parsString(resume)
+    parsedJobDes := parsString(jobDescription)
+
+    var wordCount int
+
+    for _, v := range parsedJobDes {
+        wordCount += findKeyWord(v, parsedResume) 
     }
 
-    fmt.Println(string(resume))
-    fmt.Println(string(jobDescription))
+    mached := fmt.Sprintf("%d",(wordCount/len(parsedJobDes))*100)
+
+    fmt.Println(mached+"%")
 }
